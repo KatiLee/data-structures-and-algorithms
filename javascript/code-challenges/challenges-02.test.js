29e095c7d530ea67482451ece24e86328bd2b004
KATI LEE <katherineleefoley@gmail.com>
2023-01-19 02:23:32 +0000

    maybe seven?

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -92,7 +92,8 @@ const mapTwoToThe = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7 - Stretch Goal
 
-Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each element is the result of the `charCodeAt` method on the original array element.
+Write a function named charCode that, given an array of letters as an input, uses map to return a new array where each 
+element is the result of the `charCodeAt` method on the original array element.
 
 Read the MDN documentation on String.charCodeAt() if necessary.
 
@@ -101,6 +102,10 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
+  let charArray = arr.map((value) => {
+    value.charCodeAt(0);
+  });
+  return charArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
