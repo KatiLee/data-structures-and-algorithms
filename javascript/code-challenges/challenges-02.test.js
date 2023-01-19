c8e39ece22bea93e7c003eb26d038049d91c474b
KATI LEE <katherineleefoley@gmail.com>
2023-01-19 01:02:45 +0000

    two down

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -26,6 +26,8 @@ Write a function named addOne that, given an array of numbers, uses map to retur
 
 const addOne = (arr) => {
   // Solution code here...
+  let newArray = arr.map(number => (number + 1));
+  return newArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
