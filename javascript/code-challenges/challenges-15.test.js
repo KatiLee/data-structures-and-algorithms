4a5640284030c0b975afb1fc80e9d44cc829ee42
KATI LEE <katherineleefoley@gmail.com>
2023-04-27 01:45:26 +0000

    finished the last challenge, tests passing, i was closer than i realized

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -172,7 +172,13 @@ const detectTicTacToeWin = (board) => {
       return true;
     }
   }
-  
+  if (board[0][0] !== "" && board[0][0] === board[1][1] && board[1][1] === board[2][2]){
+    return true;
+  }
+  if (board[0][2] !== "" && board[0][2] === board[1][1] && board[1][1] === board [2][0]){
+    return true;
+  }
+  return false;
 };
 
 /* ------------------------------------------------------------------------------------------------
