34a89fa690c34d9082106fc84e1ffde6db859828
KATI LEE <katherineleefoley@gmail.com>
2023-05-29 00:44:24 +0000

    have index up and two tests passing for stack

---
+++ javascript/stack-and-queue/stack.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { Stack } = require('./index');
+
+describe('Stack', () => {
+    it('can successfully push onto a stack', () => {
+        let stack = new Stack();
+        stack.push(1);
+        expect(stack.top.value).toEqual(1);
+        expect(stack.top.next).toBeNull();
+    }); 
+    it('can successfully push multiple values onto a stack', () => {
+        let stack = new Stack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        expect(stack.top.value).toEqual(3);
+        expect(stack.top.next.value).toEqual(2);
+        expect(stack.top.next.next.value).toEqual(1);
+        expect(stack.top.next.next.next).toBeNull();
+    })
+
+    // it('can successfully pop off the stack', () => {
+
+    // })
+
+});
